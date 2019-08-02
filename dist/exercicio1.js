dbe8d66c9991da43e32552fcffb9347278be2b19
Juan Thiago <thiago.reiissouza@gmail.com>
2019-08-02 20:33:26 -0300

    Exercicio 1- TypeScript

---
+++ dist/exercicio1.js
@@ -0,0 +1,11 @@
+var nota1 = 10;
+var nota2 = 5;
+var nota3 = 7;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("Sua nota é: " + media);
+if (media >= 7) {
+    console.log("Nao faz mais que sua Obrigaçao");
+}
+else {
+    console.log("Nem pra isso serve");
+}
---
+++ exercicio1.ts
@@ -0,0 +1,14 @@
+let nota1: number = 10;
+let nota2: number = 5;
+let nota3: number = 7;
+
+let media: number = (nota1 + nota2 + nota3) / 3;
+
+console.log ("Sua nota é: " + media);
+
+if (media >= 7 ) {
+    console.log ("Nao faz mais que sua Obrigaçao");
+}
+else{
+    console.log("Nem pra isso serve");
+}
